78919e15fe405f425f0fc0fe20df2a430cf36f18
Rakibul Islam <81356942+iamrakib097@users.noreply.github.com>
2024-09-02 12:44:16 +0600

    everything

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    svgr({
+      svgrOptions: {
+        icon: true, // Enable if you want to treat the SVG as an icon with custom size
+      },
+    }),
+  ],
+});
